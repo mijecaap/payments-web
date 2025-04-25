ad9e85c66c00a4660cbb49354acba8e55db19ab5
Miguel <mijecaap@hotmail.com>
2025-04-24 23:19:39 -0500

    feat: agrega servicios para integración con API (accounts, transactions, users, commissions)

---
+++ src/services/TransactionService.ts
@@ -0,0 +1,15 @@
+import { ServiceFetcher } from './ServiceFetcher';
+import { TransactionResponse, TransferRequest, TransferResponse } from '../types/transactions';
+
+export class TransactionService {
+  static async getTransactionHistory(
+    accountId: number,
+    page: number = 1,
+  ): Promise<TransactionResponse> {
+    return ServiceFetcher.get(`/transactions/${accountId}?page=${page}`);
+  }
+
+  static async transfer(data: TransferRequest): Promise<TransferResponse> {
+    return ServiceFetcher.post('/transactions/transfer', data);
+  }
+}
